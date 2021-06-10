da5f22dd0e5ebd9a75322f58023cf4d42930e298
rintse <r1ntse.a@gmail.com>
2021-06-10 13:13:51 +0000

    betere logging, leesbaarder

--- fan.cc
+++ fan.cc
@@ -6,6 +6,7 @@
 
 #define FAN_THRESH 50 // Fan enable threshold in degrees C
 #define FAN_MARGIN 4 // Switching margin
+#define POLLING_INTERVAL_SECS 3 // Polling interval in seconds
 #define TEMPS_FILE "/sys/class/thermal/thermal_zone0/temp"
 #define OUT_PIN 3 // GPIO22
 #define FAN_ON 1
@@ -27,6 +28,7 @@ int main() {
 	wiringPiSetup();
 	pinMode(OUT_PIN, OUTPUT);
 	bool run = true;
+    bool is_on = false;
 
 	while(run) {
 		// Get temp reading
@@ -34,14 +36,28 @@ int main() {
 		// Temp file logs in millidegrees
 		uint32_t temp = strtol(result.c_str(), NULL, 10) / 1000;
 
-		if(temp > FAN_THRESH + FAN_MARGIN) {
+        // Log current status
+        std::cout 
+            << "Temperature: "  << temp 
+            << " (fan is "      << (is_on ? "on" : "off") 
+            << ")"              << std::endl;
+
+        bool should_be_on = temp > FAN_THRESH + FAN_MARGIN;
+        bool should_be_off = temp < FAN_THRESH - FAN_MARGIN;
+
+		if(!is_on && should_be_on) {
+            std::cout << "Turning on fan." << std::endl;
 			digitalWrite(OUT_PIN, FAN_ON);
+            is_on = true;
 		}
-		else if(temp < FAN_THRESH - FAN_MARGIN){
+
+		else if(is_on && should_be_off){
+            std::cout << "Turning off fan." << std::endl;
 			digitalWrite(OUT_PIN, FAN_OFF);
+            is_on = false;
 		}
 		
-		std::this_thread::sleep_for(std::chrono::seconds(3));
+		std::this_thread::sleep_for(std::chrono::seconds(POLLING_INTERVAL_SECS));
 	}
 	return 0;
 }
